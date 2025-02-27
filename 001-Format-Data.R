7b65648dfa74cfd3ec2703b6164de4257487d919
swest19@lsu.edu <swest19@lsu.edu>
2025-02-27 10:38:23 -0600

    Formatting data and computing summary statistics

--- 001-Format-Data.R
+++ 001-Format-Data.R
@@ -1,19 +1,51 @@
 library(dplyr)
+library(ggplot2)
+files_appended <- readRDS("data/files_appended.rds")
 
-files <- list.files("../Intermediate Proj/Process EEG/EEG_ERP_Processing/Logfiles_append/",full.names = T)
-
-files_appended <- data.frame()
-saveRDS(files_appended, file = "data/files_appended.rds")
-for (file in files){
-  tmp <- read.table(file, header = T)
-  files_appended <- rbind(tmp,files_appended)
-}
-
+files_appended$correct <- ifelse(files_appended$correct == 2, 0, 1 )
 
 files_appended_factored <- files_appended %>%
-  mutate(cond = factor(cond, labels = , levels =),
-         button = factor(button, levels = , labels = ),
-         correct = factor(correct,levels = , labels = ),
-         block = as.factor(block),
-         counterbalance = factor(counterbalance,levels =, labels = ),
-         subno = as.factor(subno))
+  mutate(button = as.factor(button),
+         is_correct = factor(correct,levels = c(0,1) , labels =c(FALSE,TRUE)),
+         block = as.factor(ifelse(block == 1 & counterbalance == 1,"Taxonomic_inhib", ifelse(block == 2 & counterbalance == 2, "Taxonomic_inhib","Thematic_inhib"))),
+         counterbalance = factor(counterbalance,levels =c(1,2), labels =c("Tax_first", "Them_first") ),
+         PPID = as.factor(subno),
+         trial_condition = as.factor(ifelse(cond == 0 & block == "Taxonomic_inhib", 
+                                            "FillTax", 
+                                            ifelse(cond == 1 & block == "Thematic_inhib", 
+                                                   "ThemN",
+                                                   ifelse(cond == 2 & block == "Taxonomic_inhib",
+                                                          "TaxN",
+                                                          ifelse(cond == 1 & block == "Taxonomic_inhib",
+                                                                 "ThemP",
+                                                                 ifelse(cond == 2 & block == "Taxonomic_inhib",
+                                                                        "TaxP","FillThem")))))),) %>%
+  relocate(PPID, trial_condition, block, counterbalance, is_correct, button,rt,running_clock = time,order) %>%
+  select(-cond,-correct,-subno) %>%
+  group_by(PPID) %>%
+  mutate(accuracy_overall = sum(is_correct == TRUE)/n(),
+         mean_rt_overall = mean(rt)) %>%
+  group_by(block, .add = TRUE) %>%
+  mutate(accuracy_blockwise = sum(is_correct == TRUE)/n(),
+         mean_rt_blockwise = mean(rt)) %>%
+  group_by(trial_condition, .add = TRUE) %>%
+  mutate(accuracy_blockwise_trialType = sum(is_correct == TRUE)/n(),
+         mean_rt_blockwise_trialType = mean(rt))
+  
+saveRDS(files_appended_factored, file = paste0("data/","logfiles_metadata_",Sys.Date(),".rds"))
+
+
+plot_df <- files_appended_factored %>%
+  select(PPID, 
+         block, 
+         trial_condition,
+         counterbalance,
+         starts_with("accuracy"),
+         starts_with("mean")) %>%
+  unique()
+
+ggplot(plot_df, aes(x = trial_condition, y = mean_rt_blockwise_trialType, fill = block))+
+  geom_violin()
+
+
+cond, labels =c("filler", "thematic","taxonomic") , levels =c(0,1,2)
