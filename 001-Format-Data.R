f01a15b8563a89edb76d3fb18a9fbf610048c119
Swest13 <swest19@lsu.edu>
2025-02-26 20:21:42 -0600

    Initial commit

---
+++ 001-Format-Data.R
@@ -0,0 +1,19 @@
+library(dplyr)
+
+files <- list.files("../Intermediate Proj/Process EEG/EEG_ERP_Processing/Logfiles_append/",full.names = T)
+
+files_appended <- data.frame()
+
+for (file in files){
+  tmp <- read.table(file, header = T)
+  files_appended <- rbind(tmp,files_appended)
+}
+
+
+files_appended_factored <- files_appended %>%
+  mutate(cond = factor(cond, labels = , levels =),
+         button = factor(button, levels = , labels = ),
+         correct = factor(correct,levels = , labels = ),
+         block = as.factor(block),
+         counterbalance = factor(counterbalance,levels =, labels = ),
+         subno = as.factor(subno))
