a337d947515564ee2450210a6278efec3fa3a197
mgarce3 <mgarce3@lsu.edu>
2025-03-08 22:27:20 -0600

    Added box-plot with accuracy of trial condition by counterbalance. Ran ANOVA on accuracy of trial condition means.

--- 002-Analyzing-Data.R
+++ 002-Analyzing-Data.R
@@ -2,4 +2,21 @@
 #then git status (branch behind or up to date)
 
 df <- readRDS("data/logfiles_metadata_2025-03-06.rds")
+#plot_df has accuracy and rt for analysis
 
+#-----------
+#accuracy by trial condition in each block type (taxonomic_inhib and thematic_inhib) 
+#-----------
+#box plot with accuracy of trial condition by counterbalance (for fun)
+ggplot(plot_df, aes(x = trial_condition, y = accuracy_trialType, colour = trial_condition)) + 
+  geom_bar(stat = "summary", fun = "mean") + coord_cartesian(ylim = c(.60,1)) + facet_wrap(~counterbalance)
+
+#Is there a significant difference between accuracy trial type means
+
+accuracytT_model <- ezANOVA(data = plot_df,
+                            dv = accuracy_trialType,
+                            wid = PPID,
+                            within = trial_condition,
+                            detailed = TRUE)
+accuracytT_model
+#Yes, but DFd is 105... which I'm not sure if that is correct. Im going to come back to this tomorrow :)
